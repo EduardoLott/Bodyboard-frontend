30ebddef07ae0e69beec0c776f5e4c43763f8e4f
EduardoLott <dudulott@gmail.com>
2023-12-20 11:53:46 -0300

    add: home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TopbarComponent } from "../shared/topbar/topbar.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [TopbarComponent]
 })
 export class HomeComponent {
 
